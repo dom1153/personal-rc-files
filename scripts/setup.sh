b796d706dd4b0d5048df34dfb36829421467eda4
Dominic Dang <dtdd1153@live.com>
2021-11-11 09:09:44 -0600

    add linux gitconfig setup maybe

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -31,7 +31,8 @@ then
   echo "SCRIPT: Could not find folder '$expectedRCFolder' (expected to be above script location)"
   exit
 fi
-dotfiles=(.alias .alias-perforce .alias-windows .alias-local .alacritty.yml .bash_profile .bash.pathsettings .zshrc .gitconfig .gitconfig-mac-intel .gitconfig-mac-m1 .tmux.conf .vimrc .vimrc.pluginsettings)
+# consider moving this to .config dir someday
+dotfiles=(.alias .alias-perforce .alias-windows .alias-local .alacritty.yml .bash_profile .bash.pathsettings .zshrc .gitconfig .gitconfig-mac-intel .gitconfig-mac-m1 .gitconfig-linux .tmux.conf .vimrc .vimrc.pluginsettings)
 for d in ${dotfiles[*]}
 do
   if [ ! -f $d ] 
